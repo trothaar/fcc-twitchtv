0d2bc45ec8fa1284377d1676c70876d24b7460b1
Teresa <trothaar@users.noreply.github.com>
2017-02-09 11:02:04 -0500

    Add files via upload

--- twitchtv.js
+++ twitchtv.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
       'Client-ID': '4hbpaesmju932l4hqytsewsyw1s2kg'
     },
     success: function(data1){
-      console.log(data1);
+      //console.log(data1);
        if (data1.stream === null) {
       //FCC Offline
       $("#fccStatus").html("Free Code Camp is currently OFFLINE");
@@ -20,7 +20,7 @@ $(document).ready(function() {
     }
   });
 
-//Get followers without hard-coding an array as explained in Coding 360 video
+//Get followers without hard-coding an array, as explained in Coding 360 video
 $.ajax({
   type: "GET",
   url: "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/",
@@ -29,11 +29,22 @@ $.ajax({
 },
 success: function(data2){
 for (var i = 0; i < data2.follows.length; i++) {
-  //Get displayName
+  //Get displayName, logo, and status
   var displayName = data2.follows[i].channel.display_name;
   var logo = data2.follows[i].channel.logo;
   var status= data2.follows[i].channel.status;
+  //If channel does not have logo, use a placeholder
   if(logo==null){
+  logo="https://placehold.it/400x400?text=No+Logo";
+}
+// If status is null, user is offline
+if(status==null){
+  status = "User Offline";
+}
+// If displayName is undefined, user does not Exist
+if(displayName==undefined){
+  displayName = "Unknown";
+  status = "User Does Not Exist";
   logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeF9yiuuOJBNO8VpXsVp2VQIpBSTPdLKW6uB3AI-jmSX9G74bX1g";
 }
 $("#followerInfo").prepend("<div class ='row'>" + "<div class='col-md-4'>" +
@@ -46,24 +57,36 @@ $("#followerInfo").prepend("<div class ='row'>" + "<div class='col-md-4'>" +
 // Test followers that FCC gives us to ensure that code is working
 var deletedFollowers=['brunofin', 'comster404'];
 for(var i=0;i<deletedFollowers.length;i++){
+  badUserNames();
+}
+
+function badUserNames(){
+  var user = deletedFollowers[i];
+  var link = "https://api.twitch.tv/kraken/streams/" + user;
+  //console.log(link);
  $.ajax({
    type: "GET",
-   url: "https://api.twitch.tv/kraken/streams/"+deletedFollowers[i],
+   url: link,
    headers:{
   'Client-ID': '4hbpaesmju932l4hqytsewsyw1s2kg'
 },
- error: function(data3){
+ success: function(data3){
+   console.log(data3);
    var logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeF9yiuuOJBNO8VpXsVp2VQIpBSTPdLKW6uB3AI-jmSX9G74bX1g";
    var displayName= data3.statusText;
    var status= data3.status;
-     $("#followerInfo").prepend("<div class ='row'>" + "<div class='col-md-4'>" +
-          "<a  href='https://www.twitch.tv/" +displayName+  +"'><img src='" + logo + "'></a>"
-          +
-          "</div>" + "<div class='col-md-4'>" + displayName + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
+   if (displayName==undefined){
+     displayName = user;
+     status = "User Does Not Exist";
+   }
+   $("#followerInfo").prepend("<div class ='row'>" + "<div class='col-md-4'>" +
+             "<a href='http://www.twitch.tv/"+ displayName+"'><img src='" + logo + "'></a>"
+             +
+             "</div>" + "<div class='col-md-4'>" + displayName + "</div>" + "<div class='col-md-4'>" + status + "</div></div>");
  }
 });
-
 }
+//}
 
 
 });
